8417ae2ee436c6fdf06e9517b44057a953e0aeb4
Ranieri Althoff <ranisalt@gmail.com>
2025-12-18 02:50:27 -0300

    PR suggestions

--- src/tools.h
+++ src/tools.h
@@ -106,6 +106,9 @@ public:
 	explicit scope_exit(T&& func) : func_{std::move(func)} {}
 	~scope_exit() { func_(); }
 
+	scope_exit(const scope_exit&) = delete;
+	scope_exit& operator=(const scope_exit&) = delete;
+
 private:
 	T func_;
 };
