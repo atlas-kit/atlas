19483f2ae8d6a26a50a3bc61bf1c40a4f68e8163
ramon-bernardo <ramonbernardo.contato@gmail.com>
2025-12-24 12:09:34 -0300

    fix(events): remove invalid event

--- data/events/scripts/player.lua
+++ data/events/scripts/player.lua
@@ -61,13 +61,6 @@ function Player:onReportRuleViolation(targetName, reportType, reportReason, comm
 	end
 end
 
-function Player:onReportBug(message, position)
-	if Event.onReportBug then
-		Event.onReportBug(self, message, position)
-	end
-	return true
-end
-
 function Player:onRotateItem(item)
 	local onRotateItem = EventCallback.onRotateItem
 	if onRotateItem then
