db52237e5d060c7c3aaf785d907209b4461a7b2f
Luan Luciano <6209529+luanluciano93@users.noreply.github.com>
2025-12-24 15:10:17 -0300

    update

--- src/protobuf/appearances.cpp
+++ src/protobuf/appearances.cpp
@@ -1,5 +1,5 @@
-// Copyright 2024 The Atlas Server Authors.
-// Appearances loader for Tibia Client 12+ format
+// Copyright 2023 The Forgotten Server Authors. All rights reserved.
+// Use of this source code is governed by the GPL-2.0 License that can be found in the LICENSE file.
 
 #include "otpch.h"
 #include "appearances.h"
--- src/protobuf/appearances.h
+++ src/protobuf/appearances.h
@@ -1,5 +1,5 @@
-// Copyright 2024 The Atlas Server Authors.
-// Appearances loader for Tibia Client 12+ format
+// Copyright 2023 The Forgotten Server Authors. All rights reserved.
+// Use of this source code is governed by the GPL-2.0 License that can be found in the LICENSE file.
 
 #ifndef FS_APPEARANCES_H
 #define FS_APPEARANCES_H
