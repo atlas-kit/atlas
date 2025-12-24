5f5ba6d7bda233ba01ce5c4e6b47fd0d32f1ef98
Luan Luciano <6209529+luanluciano93@users.noreply.github.com>
2025-12-24 15:40:52 -0300

    update

--- src/protobuf/appearances.cpp
+++ src/protobuf/appearances.cpp
@@ -1,13 +1,14 @@
 // Copyright 2023 The Forgotten Server Authors. All rights reserved.
 // Use of this source code is governed by the GPL-2.0 License that can be found in the LICENSE file.
 
-#include "otpch.h"
+#include "../otpch.h"
+
 #include "appearances.h"
+
+#include "appearances.pb.h"
 
 #include <fstream>
 #include <iostream>
-
-#include "appearances.pb.h"
 
 Appearances g_appearances;
 
