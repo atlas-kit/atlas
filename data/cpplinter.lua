63ea4fead05c2a090f8f328c59ab05982f4a1c42
ramon-bernardo <ramonbernardo.contato@gmail.com>
2025-12-24 12:00:02 -0300

    fix(lua linter): on look returns

--- data/cpplinter.lua
+++ data/cpplinter.lua
@@ -1029,10 +1029,10 @@ Weapon = {}
 ---@field onInvite fun(party:Party, player:Player):boolean
 ---@field onRevokeInvitation fun(party:Party, player:Player):boolean
 ---@field onPassLeadership fun(party:Party, player:Player):boolean
----@field onLook fun(player:Player, thing:Thing, position:Position, distance:integer):string
----@field onLookInBattleList fun(player:Player, creature:Creature, distance:integer):string
----@field onLookInTrade fun(player:Player, partner:Player, item:Item, distance:integer):string
----@field onLookInShop fun(player:Player, itemType:ItemType, count:integer):string
+---@field onLook fun(player:Player, thing:Thing, position:Position, distance:integer):nil
+---@field onLookInBattleList fun(player:Player, creature:Creature, distance:integer):nil
+---@field onLookInTrade fun(player:Player, partner:Player, item:Item, distance:integer):nil
+---@field onLookInShop fun(player:Player, itemType:ItemType, count:integer):nil
 ---@field onMoveItem fun(player:Player, item:Item, count:integer, fromPosition:Position, toPosition:Position, fromThing?:Thing, toThing?:Thing):integer
 ---@field onItemMoved fun(player:Player, item:Item, count:integer, fromPosition:Position, toPosition:Position, fromThing?:Thing, toThing?:Thing):nil
 ---@field onMoveCreature fun(player:Player, creature:Creature, fromPosition:Position, toPosition:Position):boolean
