a90932fb21906db7357dcc0470516833cf220029
ramon-bernardo <ramonbernardo.contato@gmail.com>
2025-12-27 13:27:11 -0300

    fix(events): remove self decl

--- data/scripts/events/game.lua
+++ data/scripts/events/game.lua
@@ -1,17 +1,17 @@
 function Game:onStartup()
 	if Event.onGameStartup then
-		Event.onGameStartup(self)
+		Event.onGameStartup()
 	end
 end
 
 function Game:onShutdown()
 	if Event.onGameShutdown then
-		Event.onGameShutdown(self)
+		Event.onGameShutdown()
 	end
 end
 
 function Game:onSave()
 	if Event.onGameSave then
-		Event.onGameSave(self)
+		Event.onGameSave()
 	end
 end
