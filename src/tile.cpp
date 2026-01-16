6682c4b57a222b638e9cf997a6799a1c2159f791
Luan Luciano <6209529+luanluciano93@users.noreply.github.com>
2026-01-15 22:48:36 -0300

    update

--- src/tile.cpp
+++ src/tile.cpp
@@ -1055,10 +1055,11 @@ void Tile::removeThing(const std::shared_ptr<Thing>& thing, uint32_t count)
 			return;
 		}
 
-		std::vector<int32_t> oldStackPosVector;
-
 		SpectatorVec spectators;
 		g_game.map.getSpectators(spectators, getPosition(), true);
+
+		std::vector<int32_t> oldStackPosVector;
+		oldStackPosVector.reserve(spectators.size());
 		for (const auto& spectator : spectators) {
 			if (const auto& spectatorPlayer = spectator->asPlayer()) {
 				oldStackPosVector.push_back(getStackposOfItem(spectatorPlayer, item));
@@ -1076,10 +1077,11 @@ void Tile::removeThing(const std::shared_ptr<Thing>& thing, uint32_t count)
 			item->setItemCount(newCount);
 			onUpdateTileItem(item, itemType, item, itemType);
 		} else {
-			std::vector<int32_t> oldStackPosVector;
-
 			SpectatorVec spectators;
 			g_game.map.getSpectators(spectators, getPosition(), true);
+
+			std::vector<int32_t> oldStackPosVector;
+			oldStackPosVector.reserve(spectators.size());
 			for (const auto& spectator : spectators) {
 				if (const auto& spectatorPlayer = spectator->asPlayer()) {
 					oldStackPosVector.push_back(getStackposOfItem(spectatorPlayer, item));
