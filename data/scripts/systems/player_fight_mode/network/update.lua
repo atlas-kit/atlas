45425aa57adc29ed0712ec9ec10741b2f09a8c10
Ranieri Althoff <1993083+ranisalt@users.noreply.github.com>
2026-02-13 19:37:18 +0100

    Fix indentation in update.lua

--- data/scripts/systems/player_fight_mode/network/update.lua
+++ data/scripts/systems/player_fight_mode/network/update.lua
@@ -1,9 +1,9 @@
 local handler = PacketHandler(0xA0)
 
 function handler.onReceive(player, msg)
-    local fightModeRaw = msg:getByte() -- 1 - offensive, 2 - balanced, 3 - defensive
-    local chaseModeRaw = msg:getByte() -- 0 - stand while fighting, 1 - chase opponent
-    local secureModeRaw = msg:getByte() -- 0 - can't attack unmarked, 1 - can attack unmarked
+	local fightModeRaw = msg:getByte() -- 1 - offensive, 2 - balanced, 3 - defensive
+	local chaseModeRaw = msg:getByte() -- 0 - stand while fighting, 1 - chase opponent
+	local secureModeRaw = msg:getByte() -- 0 - can't attack unmarked, 1 - can attack unmarked
 	local pvpModeRaw = msg:getByte() -- pvp mode introduced in 10.0
 
 	local fightMode = FIGHTMODE_DEFENSE
