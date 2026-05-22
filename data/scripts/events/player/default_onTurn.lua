0c2fee9d8f0157aa1e40de630ba7249b7dc0ced9
ramon-bernardo <ramonbernardo.contato@gmail.com>
2026-05-22 20:21:06 -0300

    refactor: use addBool instead of addByte for walkthrough

--- data/scripts/events/player/default_onTurn.lua
+++ data/scripts/events/player/default_onTurn.lua
@@ -61,7 +61,7 @@ local function sendCreatureTurn(player, creature, stackpos, direction)
     msg:addU16(0x63)
     msg:addU32(creature:getId())
     msg:addByte(direction)
-    msg:addByte(canWalkthroughEx(player, creature) and 0 or 1)
+    msg:addBool(not canWalkthroughEx(player, creature))
     return msg:sendToPlayer(player)
 end
 
