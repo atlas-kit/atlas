c30a4147c6ff48fa1735ef090fa14859c032a924
github-action-benchmark <github@users.noreply.github.com>
2026-06-06 23:50:12 +0000

    add Benchmark (googlecpp) benchmark result for d54007bf23cc18bbb2723999d574d0edf50bd1fe

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1780786751474,
+  "lastUpdate": 1780789811990,
   "repoUrl": "https://github.com/atlas-kit/atlas",
   "entries": {
     "Benchmark": [
@@ -80,6 +80,90 @@ window.BENCHMARK_DATA = {
             "value": 3935.5405548790313,
             "unit": "ns/iter",
             "extra": "iterations: 356258\ncpu: 3935.259244704675 ns\nthreads: 1"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "ramonbernardo.contato@gmail.com",
+            "name": "Ramon Bernardo",
+            "username": "ramon-bernardo"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "d54007bf23cc18bbb2723999d574d0edf50bd1fe",
+          "message": "feat: add benchmark support (#353)",
+          "timestamp": "2026-06-06T20:34:49-03:00",
+          "tree_id": "3aa3531a22866fc9ede85f87611d10070747db12",
+          "url": "https://github.com/atlas-kit/atlas/commit/d54007bf23cc18bbb2723999d574d0edf50bd1fe"
+        },
+        "date": 1780789811312,
+        "tool": "googlecpp",
+        "benches": [
+          {
+            "name": "bench_base64_encode/1",
+            "value": 471.4839737510059,
+            "unit": "ns/iter",
+            "extra": "iterations: 2973372\ncpu: 471.36196614483487 ns\nthreads: 1"
+          },
+          {
+            "name": "bench_base64_encode/8",
+            "value": 511.6689905833811,
+            "unit": "ns/iter",
+            "extra": "iterations: 2733460\ncpu: 511.56724334725953 ns\nthreads: 1"
+          },
+          {
+            "name": "bench_base64_encode/64",
+            "value": 584.5398246291768,
+            "unit": "ns/iter",
+            "extra": "iterations: 2446815\ncpu: 584.501968477388 ns\nthreads: 1"
+          },
+          {
+            "name": "bench_base64_encode/512",
+            "value": 904.4195452487065,
+            "unit": "ns/iter",
+            "extra": "iterations: 1545900\ncpu: 904.3529885503588 ns\nthreads: 1"
+          },
+          {
+            "name": "bench_base64_encode/1024",
+            "value": 1305.6590611460435,
+            "unit": "ns/iter",
+            "extra": "iterations: 1074395\ncpu: 1305.5893577315617 ns\nthreads: 1"
+          },
+          {
+            "name": "bench_base64_decode/1",
+            "value": 442.37325928249527,
+            "unit": "ns/iter",
+            "extra": "iterations: 3167013\ncpu: 442.3377185379414 ns\nthreads: 1"
+          },
+          {
+            "name": "bench_base64_decode/8",
+            "value": 465.367456946019,
+            "unit": "ns/iter",
+            "extra": "iterations: 3006110\ncpu: 465.340890386579 ns\nthreads: 1"
+          },
+          {
+            "name": "bench_base64_decode/64",
+            "value": 677.5012056216791,
+            "unit": "ns/iter",
+            "extra": "iterations: 2057030\ncpu: 677.4634516754743 ns\nthreads: 1"
+          },
+          {
+            "name": "bench_base64_decode/512",
+            "value": 2196.2444244352037,
+            "unit": "ns/iter",
+            "extra": "iterations: 638590\ncpu: 2196.1653909394145 ns\nthreads: 1"
+          },
+          {
+            "name": "bench_base64_decode/1024",
+            "value": 3930.873557771696,
+            "unit": "ns/iter",
+            "extra": "iterations: 356653\ncpu: 3930.6873459637227 ns\nthreads: 1"
           }
         ]
       }
